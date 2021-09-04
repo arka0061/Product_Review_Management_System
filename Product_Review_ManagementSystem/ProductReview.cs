dd108a34c063e43240618a359ab0bff04f23d60f
Arka <arka.spectacular@gmail.com>
2021-09-04 08:43:36 +0530

    [Arka]Add:Product review Class Added

---
+++ Product_Review_ManagementSystem/ProductReview.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Product_Review_ManagementSystem
+{
+    public class ProductReview
+    {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public int Rating { get; set; }
+        public string Review { get; set; }
+        public string IsLike { get; set; }
+
+    }
+}
